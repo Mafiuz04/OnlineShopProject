81ccf5f3f444b411ab0d720393e47e0e95984880
Mafiuz04 <mathew.modrzynski@gmailc.om>
2024-03-14 19:35:53 +0100

    Add Smartphone and Accessory class

---
+++ src/entity/Accessory.java
@@ -0,0 +1,50 @@
+package entity;
+
+import java.util.Objects;
+
+public class Accessory extends Smartphone{
+    private String name;
+    private int price;
+
+    public Accessory(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Accessory accessory = (Accessory) o;
+        return price == accessory.price && Objects.equals(name, accessory.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Accessory{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
