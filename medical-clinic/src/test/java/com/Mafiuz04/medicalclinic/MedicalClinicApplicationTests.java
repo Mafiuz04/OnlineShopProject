c571bf9be29bba05d1baf61025a21c045bef9da5
Mafiuz04 <mathew.modrzynski@gmailc.om>
2024-04-25 22:25:48 +0200

    App

---
+++ medical-clinic/src/test/java/com/Mafiuz04/medicalclinic/MedicalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mafiuz04.medicalclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
